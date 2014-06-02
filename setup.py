23af8b110e5312902d534aa9407993124e0c9aa1
z.sean.huang <z.sean.huang@gmail.com>
2014-06-02 08:40:28 -0700

    [vtapi] - add setup.py, enable pip install vtapi

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='vtapi',
+      version='1.4',
+      description='this library implement virustotal api v2.0',
+      author='z.sean.huang',
+      author_email='z.sean.huang@gmail.com',
+      url='http://github.com/z-sean-huang/VirustotalAPI',
+      py_modules=['vtapi'],
+      license='MIT',
+      platforms=['Any'],
+      keywords=["virustotal", "malicious", "virus"],
+      requires=['requests'],
+)
